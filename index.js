7d5150dd68261caae273063ae4758a2aed55bc8d
Nhi Eudy <nhieudy@Nhis-MacBook-Pro.local>
2025-11-12 15:25:07 -0600

    added geocodingAPI, saved lat and lon values

--- index.js
+++ index.js
@@ -1,28 +1,49 @@
 let form = document.querySelector("form");
 let h2 = document.querySelector("h2");
+let main = document.querySelector("main");
 const PET_KEY = "petName";
-const ZIP_KEY = "zipCode"
+const ZIP_KEY = "zipCode";
+const LAT_KEY = "lat";
+const LON_KEY = "lon";
 let pet = localStorage.getItem(PET_KEY);
-
 let previousName = localStorage.getItem("petName");
 let previousZIP = localStorage.getItem("zipCode");
+const url = `http://api.openweathermap.org/geo/1.0/zip?zip=`;
+const apikey = `&appid=61ac71fca852832313e86693bf383076`;
 
 if (previousName) {
   h2.textContent = `Hello, ${previousName}!`;
-}
-else{
-    h2.textContent = `Hello, Fido!`;
+} else {
+  h2.textContent = `Hello, Fido!`;
 }
 
-form.onsubmit = (e) => {
+form.onsubmit = async (e) => {
   e.preventDefault();
-  let name = form.petName.value;
+  const zipCode = form.zipCode.value;
+  try {
+    const res = await fetch(url + zipCode + apikey);
+    const geoData = await res.json();
+    if (res.ok) {
+      alert("Info saved!");
+      latLon(geoData);
+    } else {
+      alert("Invalid Zip Code!");
+    }
+  } catch (err) {
+    alert("Incorrect Inputs!");
+  }
+};
+
+const latLon = (geoData) => {
+  let name = capitalizeString(form.petName.value);
   let zipCode = form.zipCode.value;
-  name = capitalizeString(name);
+  const lat = geoData.lat;
+  const lon = geoData.lon;
   localStorage.setItem(PET_KEY, name);
   localStorage.setItem(ZIP_KEY, zipCode);
+  localStorage.setItem(LAT_KEY, lat);
+  localStorage.setItem(LON_KEY, lon);
   h2.textContent = `Hello, ${name}!`;
-  console.log(name);
   form.reset();
 };
 
--- weather.js
+++ weather.js
@@ -1,25 +1,32 @@
-/*const url = `http://api.openweathermap.org/geo/1.0/zip?zip=73170&appid=61ac71fca852832313e86693bf383076`;
+const previousZIP = localStorage.getItem("zipCode");
+const main = document.querySelector("main");
+const urlZip = `http://api.openweathermap.org/geo/1.0/zip?zip=${previousZIP}&appid=61ac71fca852832313e86693bf383076`;
+// fetch(url) // make the request
+// .then(function(res) {
+//   return res.json() // when the response is received, convert to json
+// })
+// .then(function(res) {
+//   console.log(res) // when the json is converted, log it
+// })
+// .catch(function(err) {
+//   console.log(err)
+// })
 
-fetch(url) // make the request
-.then(function(res) {
-  return res.json() // when the response is received, convert to json
-})
-.then(function(res) {
-  console.log(res) // when the json is converted, log it
-})
-.catch(function(err) {
-  console.log(err)
-})*/
+geoCode(urlZip);
 
-/*const url = `https://dogapi.dog/api/v1/facts?number=2?`;
+const geoCode = async (url) => {
+  try {
+    const res = await fetch(url);
+    const geoData = await res.json();
+    latLon(geoData);
+  } catch (err) {
+    const wrong = document.createElement(`h2`);
+    wrong.textContent = "Incorrect Zip Code";
+    main.appendChild(wrong);
+  }
+};
 
-fetch(url) // make the request
-.then(function(res) {
-  return res.json() // when the response is received, convert to json
-})
-.then(function(res) {
-  console.log(res) // when the json is converted, log it
-})
-.catch(function(err) {
-  console.log(err)
-})*/
+const latLon = (geoData) => {
+console.log(geoData)
+}
+
